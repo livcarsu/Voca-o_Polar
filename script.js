d8f5ee78101c8ef0496732db36e26f7d213245dc
livcarsu <132684337+livcarsu@users.noreply.github.com>
2024-08-21 17:42:42 -0300

    script.js

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const body = document.body;
-    const cursorBall = document.getElementById('cursorBall');
-    const cursorMargin = 50; // Margem para ativar o cursor grande
 
-    body.addEventListener('mousemove', (event) => {
-        const { clientX} = event;
-        const { innerWidth} = window;
-
-        cursorBall.style.left = `${clientX}px`;
-   
-
-        if (clientX < cursorMargin || clientX > innerWidth - cursorMargin) {
-            body.classList.add('large-circular-cursor');
-        } else {
-            body.classList.remove('large-circular-cursor');
-        }
-    });
-});
 
 function showNextModal(modalId) {
     const modals = document.querySelectorAll('.modal');
