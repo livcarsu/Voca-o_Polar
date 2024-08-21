09d149d28b95ded4a37f0be3638fb86071d22e1a
mari215 <132685774+mari215@users.noreply.github.com>
2024-08-21 11:54:11 -0300

    script.js

--- script.js
+++ script.js
@@ -18,4 +18,25 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+function showNextModal(modalId) {
+    const modals = document.querySelectorAll('.modal');
+    modals.forEach(modal => modal.style.display = 'none');
+    
+    const nextModal = document.getElementById(modalId);
+    if (nextModal) {
+        nextModal.style.display = 'flex';
+    }
+}
+
+function closeModal(modalId) {
+    const modal = document.getElementById(modalId);
+    if (modal) {
+        modal.style.display = 'none';
+    }
+}
+
+// Exibe a primeira modal ao clicar no botão "INICIAR TESTE"
+document.querySelector('.btnIniciar').addEventListener('click', function() {
+    showNextModal('modal1');
+});
 
