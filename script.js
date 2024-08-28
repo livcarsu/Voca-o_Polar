2f00e51346c2a1da3c08a2f374fa575f20e8eb6b
mari215 <132685774+mari215@users.noreply.github.com>
2024-08-28 11:54:06 -0300

    script.js

--- script.js
+++ script.js
@@ -10,15 +10,17 @@ function showNextModal(modalId) {
     }
 }
 
-function closeModal(modalId) {
-    const modal = document.getElementById(modalId);
-    if (modal) {
-        modal.style.display = 'none';
-    }
-}
-
 // Exibe a primeira modal ao clicar no botão "INICIAR TESTE"
 document.querySelector('.btnIniciar').addEventListener('click', function() {
     showNextModal('modal1');
 });
 
+function closeModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "none";
+
+    // Redireciona para a página de jogo após o fechamento do modal
+    if (modalId === 'modal3') {
+        window.location.href = 'pagina-de-jogo.html'; // Substitua 'pagina-de-jogo.html' pelo caminho da sua página de jogo
+    }
+}
